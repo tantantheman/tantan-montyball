f75b0a3ce711ce40d868ac8181b3002a069bff07
TanTan Wang <tantan.wang@yale.edu>
2019-12-12 01:38:54 -0500

    lcd Working

--- montyball/montyball.ino
+++ montyball/montyball.ino
@@ -9,12 +9,15 @@ Servo twoServo;
 int servoOnePosition = 0;
 int servoTwoPosition = 0;
 
+const int buttonPin = 34;
+
 int gameStarted = 0;
 
 int highScore = 0;
 
 int scoreCount = 0;
 
+int buttonState = 0;
 
 //the initialHiddenPositions when the game is not active
 int servoOneHidden = 0;
@@ -28,17 +31,20 @@ void setup() {
   oneServo.write(servoOnePosition);
   twoServo.write(servoTwoPosition);
 
+  pinMode(buttonPin, INPUT_PULLUP);
+
   lcd.init();  //initialize the lcd
   lcd.backlight();  //open the backlight 
  
   lcd.setCursor ( 0, 0 );            // go to the top left corner
   lcd.print("  MONTY BALL GAME   "); // write this string on the top row
   lcd.setCursor ( 0, 1 );            // go to the 2nd row
-  lcd.print("High Score: "); // pad string with spaces for centering
+  lcd.print("  High Score: ");
+  lcd.print(highScore);              // pad string with spaces for centering
   lcd.setCursor ( 0, 2 );            // go to the third row
-  lcd.print("   TO PLAY:         "); // pad with spaces for centering
+  lcd.print("              "); // pad with spaces for centering
   lcd.setCursor ( 0, 3 );            // go to the fourth row
-  lcd.print("   ROLL FIRST BALL  ");
+  lcd.print(" TO PLAY: ROLL BALL ");
 }
 
 /*
@@ -50,7 +56,28 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+buttonState = digitalRead(buttonPin);
+
+if (buttonState == 1 && gameStarted == 0)
+{
+  gameStarted = 1;
+}
+if (buttonState == 1 && gameStarted == 1)
+{
+  lcd.setCursor (0, 0);
+  lcd.print("");
+  lcd.setCursor (0, 1);
+  lcd.print("     GAME OVER      ");
+  lcd.setCursor (0, 2);
+  lcd.print("");
+  lcd.setCursor (0, 3);
+  lcd.print("");
+
+  delay(100);
+  returnHomeScreen();
+}
+  
 if (gameStarted == 0) //offline  
 {
   //set LCD to write original screen
@@ -59,8 +86,29 @@ if (gameStarted == 0) //offline
 if (gameStarted == 1)
 {
   //start game
+  lcd.setCursor ( 0, 0 );            // go to the top left corner
+  lcd.print("  MONTY BALL GAME   "); // write this string on the top row
+  lcd.setCursor ( 0, 1 );            // go to the 2nd row
+  lcd.print("                    "); // pad string with spaces for centering
+  lcd.setCursor ( 0, 2 );            // go to the third row
+  lcd.print("Score: ");
+  lcd.print(scoreCount); // pad with spaces for centering
+  lcd.setCursor ( 0, 3 );            // go to the fourth row
+  lcd.print("                    ");
   
 }
 
+}
 
+void returnHomeScreen()
+{
+   lcd.setCursor ( 0, 0 );            // go to the top left corner
+  lcd.print("  MONTY BALL GAME   "); // write this string on the top row
+  lcd.setCursor ( 0, 1 );            // go to the 2nd row
+  lcd.print("  High Score: ");
+  lcd.print(highScore);              // pad string with spaces for centering
+  lcd.setCursor ( 0, 2 );            // go to the third row
+  lcd.print("              "); // pad with spaces for centering
+  lcd.setCursor ( 0, 3 );            // go to the fourth row
+  lcd.print(" TO PLAY: ROLL BALL ");
 }
