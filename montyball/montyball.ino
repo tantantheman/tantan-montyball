d79564ab5690e98eecf51b06d4cc49863649ee36
TanTan Wang <tantan.wang@yale.edu>
2019-12-12 12:04:40 -0500

    fixed bugs

--- montyball/montyball.ino
+++ montyball/montyball.ino
@@ -13,6 +13,8 @@ const int buttonPin = 34;
 const int piezoPin = 4;
 
 int buttonCount = 0;
+int myPos[] = {10, 30, 50, 70, 90, 110, 130, 150}; 
+int moving = 0;
 
 int gameStarted = 0;
 
@@ -34,7 +36,7 @@ long previousMillis3 = 0;        // will store last time LED 3 was updated
 
 long timeHeartbeat = 500;        // time between flashes of LED 1, as a sign of life  (milliseconds)
 long timeWarning = 5000;         // time before warning light starts flashing (milliseconds)
-long timeExpired = 50000;        // time at which last LED turns on (milliseconds)
+long timeExpired = 60000;        // time at which last LED turns on (milliseconds)
 
 boolean beating = true;          // status of each process for easy boolean checks
 boolean warning = false;         // status of each process for easy boolean checks
@@ -54,7 +56,7 @@ void setup() {
 
   lcd.init();  //initialize the lcd
   lcd.backlight();  //open the backlight 
- 
+  
   lcd.setCursor ( 0, 0 );            // go to the top left corner
   lcd.print("  MONTY BALL GAME   "); // write this string on the top row
   lcd.setCursor ( 0, 1 );            // go to the 2nd row
@@ -84,17 +86,22 @@ if (piezoState == 1 && gameStarted == 0)
 {
   gameStarted = 1;
 
-  //delay(5000);
-
 }
 
-  
-if (gameStarted == 0) //offline  
+if (gameStarted == 0)
 {
-  //set LCD to write original screen
-  returnHomeScreen();
-
+  
+  lcd.setCursor ( 0, 0 );            // go to the top left corner
+  lcd.print("  MONTY BALL GAME   "); // write this string on the top row
+  lcd.setCursor ( 0, 1 );            // go to the 2nd row
+  lcd.print("  High Score: ");
+  lcd.print(highScore);              // pad string with spaces for centering
+  lcd.setCursor ( 0, 2 );            // go to the third row
+  lcd.print("              "); // pad with spaces for centering
+  lcd.setCursor ( 0, 3 );            // go to the fourth row
+  lcd.print(" TO PLAY: ROLL BALL ");
 }
+
 
 if (gameStarted == 1)
 {
@@ -112,16 +119,25 @@ if (gameStarted == 1)
   if (piezoState == 1)
   {
     scoreCount++;
+  newPos = (int)random(0, 7);
+  oneServo.write(myPos[newPos]);
+  newPos = (int)random(0, 7);
+  twoServo.write(myPos[newPos]);  
   }
-  newPos = random(0, 180);
-  oneServo.write(newPos);
-  newPos = random(0, 180);
-  twoServo.write(newPos);
+
 
 }
 
 if (gameStarted == 2)
 {
+    gameStarted = 0;
+
+    if (scoreCount > highScore)
+  {
+    highScore = scoreCount; 
+  }
+  oneServo.write(servoOnePosition);
+  twoServo.write(servoTwoPosition);
   lcd.setCursor ( 0, 0 );            // go to the top left corner
   lcd.print("  GAME OVER         "); // write this string on the top row
   lcd.setCursor ( 0, 1 );            // go to the 2nd row
@@ -131,10 +147,19 @@ if (gameStarted == 2)
   lcd.print(scoreCount); // pad with spaces for centering
   lcd.setCursor ( 0, 3 );            // go to the fourth row
   lcd.print("                    ");
-
   delay(10000);
 
-  gameStarted == 0;
+  scoreCount = 0;
+  previousMillis = 0;         // will store last time LED 1 was updated
+  previousMillis2 = 0;        // will store last time LED 2 was updated
+  previousMillis3 = 0;        // will store last time LED 3 was updated
+  
+  beating = true;          // status of each process for easy boolean checks
+  warning = false;         // status of each process for easy boolean checks
+  expired = false;
+
+  //Serial.println(gameStarted);
+
 }
 
  // check to see if it's time to blink the LED; that is, is the difference
@@ -143,16 +168,7 @@ if (gameStarted == 2)
  
  // Check to see if it is time to flash the HEARTBEAT
  if (millis() - previousMillis > timeHeartbeat){
-     // if the LED is off turn it on and vice-versa,
-     // making it flash at the interval set in timeHeartbeat
-    
-     if (beating == true){
-    // digitalWrite(ledPin, value);
-     }
-    if ((warning == true) and (expired == false)) {
-      // digitalWrite(ledPin2, value);
-    }
-   
+
      // Check to see if it is time to turn on the WARNING LED
      if (millis() - previousMillis2 > timeWarning) {
        warning = true;     // set flag so warning LED will start flashing with heartbeat
@@ -163,10 +179,10 @@ if (gameStarted == 2)
        if(millis() - previousMillis3 > timeExpired){
          expired = true;   // set flag so warning LED won't flash anymore
          beating = false;  // set flag so heartbeat LED won't flash anymore
-       //  digitalWrite(ledPin3, HIGH);  // set final state of LED        
-        // digitalWrite(ledPin2, HIGH);  // set final state of LED
-       //  digitalWrite(ledPin, HIGH);   // set final state of LED 
-       gameStarted = 2;                   
+         if (gameStarted == 1)
+         {
+         gameStarted = 2;   
+         }               
          previousMillis3 = millis();   // remember the last time we blinked the LED
        }
      previousMillis2 = millis();       // remember the last time we blinked the LED
@@ -174,17 +190,4 @@ if (gameStarted == 2)
      previousMillis = millis();        // remember the last time we blinked the LED
  }
 
-}
-
-void returnHomeScreen()
-{
-   lcd.setCursor ( 0, 0 );            // go to the top left corner
-  lcd.print("  MONTY BALL GAME   "); // write this string on the top row
-  lcd.setCursor ( 0, 1 );            // go to the 2nd row
-  lcd.print("  High Score: ");
-  lcd.print(highScore);              // pad string with spaces for centering
-  lcd.setCursor ( 0, 2 );            // go to the third row
-  lcd.print("              "); // pad with spaces for centering
-  lcd.setCursor ( 0, 3 );            // go to the fourth row
-  lcd.print(" TO PLAY: ROLL BALL ");
 }
